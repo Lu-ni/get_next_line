2f598911c27b099b5f991013ce4ca2dccadca478
lnicolli <lnicolli@c2r9s4.42lausanne.ch>
2023-11-06 20:49:21 +0100

    starting fresh

---
+++ main.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include "get_next_line.h"
+
+int main(int argc, char **argv) {
+    int fd;
+    char *line;
+
+    // Check for proper usage
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s <filename>\n", argv[0]);
+        return 1;
+    }
+
+    // Open the file
+    fd = open(argv[1], O_RDONLY);
+    if (fd == -1) {
+        perror("Error opening file");
+        return 1;
+    }
+
+    // Read lines until get_next_line returns NULL
+    while ((line = get_next_line(fd)) != NULL) {
+        printf("%s", line);
+    }
+
+    // Close the file
+    close(fd);
+
+    return 0;
+}
+
